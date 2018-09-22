0a4c5296aa2ad976f73ef0757575761d80491366
thePeter876 <thePeter876@gmail.com>
2018-09-22 12:59:56 +0200

    He realizado algunos cambios y el seguimiento suave de la cámara ya va tomando forma. Aún está lejos de ser terminado.

--- Assets/Scripts/Camera/PlayerCameraScript.cs
+++ Assets/Scripts/Camera/PlayerCameraScript.cs
@@ -7,21 +7,34 @@ public class PlayerCameraScript : MonoBehaviour {
     public GameObject pc_mainObject;    //Variable que apunta al objeto a seguir
     private Transform pc_ObjFollowed;   //Variable que guarda el transform del objeto a seguir
     Matrix4x4 pc_B = new Matrix4x4();   //Variable que guarda la matriz de cambio de base
+    public float pc_distanceFromObject;
+    public float pc_CameraHeight;
+    Vector3 pc_NewPos = new Vector3(0, 0, 0);
+    Vector3 pc_LastPos = new Vector3(0, 0, 0);
+    public float pc_MaxAngle = 110f;
 
     void Awake () {
         pc_ObjFollowed = pc_mainObject.transform;   //Guardo el transform del objeto a seguir en la variable pc_ObjFollowed
+        pc_distanceFromObject = 3.5f;
+        pc_CameraHeight = 0.5f;
     }
 	
+
+
 	void LateUpdate () {
-        placeAt(pc_ObjFollowed,new Vector3(0.0f, 0.0f, -3.0f)); //Coloco a la cámara a una distancia cierta respecto del objeto a seguir
+        Base(pc_ObjFollowed);
+        ChangeBase("toObject");
+        //pc_NewPos = transform.position;
+        placeAt(NewCameraPos(pc_ObjFollowed));
         transform.LookAt(pc_ObjFollowed);
+        pc_mainObject.transform.Rotate(90, 0, 0);
     }
     void Base(Transform newBase)    //Método que calcula la matriz de cambio de base para pasar la cámara a coordenadas del objeto a seguir
     {
         int pc_MatrixSize = 4;  //Defino el tamaño de la matriz, su única utilidad es poner límite a los bucles for
         Vector4[] pc_Columnes = new Vector4[pc_MatrixSize]; //Defino un array de Vector4 que guardará las columnas de la matriz de cambio de base
         
-        //Doy valor a las columnas en base a la fórmula para calcular la matriz de cambio de base
+        //Doy valor a las columnas según la fórmula para calcular la matriz de cambio de base
         pc_Columnes[0] = new Vector4(newBase.right.x, newBase.right.y, newBase.right.z, 0.0f);
         pc_Columnes[1] = new Vector4(newBase.up.x, newBase.up.y, newBase.up.z, 0.0f);
         pc_Columnes[2] = new Vector4(newBase.forward.x, newBase.forward.y, newBase.forward.z, 0.0f);
@@ -38,11 +51,44 @@ public class PlayerCameraScript : MonoBehaviour {
         else if (pc_direction.Equals("toObject"))   //en este modo llevamos a la cámara a coordenadas del objeto
             transform.position = pc_B.inverse.MultiplyPoint(transform.position);
     }
-    void placeAt (Transform pc_reference, Vector3 pc_NewPosition)   //Método que me permite colocar un objeto en una nueva posición respecto a la base de otro objeto
+    void placeAt (Vector3 pc_NewPosition)   //Método que me permite colocar un objeto en una nueva posición respecto a la base de otro objeto
     {
-        Base(pc_reference);
-        ChangeBase("toObject");
+        //Base(pc_reference);
+        //ChangeBase("toObject");
         transform.position = pc_NewPosition;
         ChangeBase("toWorld");
+    }
+    Vector3 NewCameraPos(Transform Obj)
+    {
+        /*/Vector3 pc_NewPos = new Vector3(0, 0, 0);
+        Vector3 ObjDirection = Obj.forward.normalized;
+
+        pc_NewPos.x = ObjDirection.x;
+        pc_NewPos.y = pc_CameraHeight;
+        pc_NewPos.z = -ObjDirection.z;
+
+        pc_NewPos *= pc_distanceFromObject;/**/
+
+        /*/Vector3 pc_NewPos = transform.position;
+        pc_NewPos = (pc_distanceFromObject * pc_NewPos) / pc_NewPos.magnitude;/**/
+        Vector3 pc_ObjVector = Vector3.ProjectOnPlane(Obj.forward,new Vector3(0, 1, 0));
+        Vector3 pc_CameraVector = Vector3.ProjectOnPlane(transform.forward, new Vector3(0, 1, 0));
+        float pc_angle = Vector3.Angle(pc_CameraVector, pc_ObjVector);
+        if (pc_angle > pc_MaxAngle) //La cámara no debe girar
+        {
+            pc_NewPos = pc_LastPos;
+        } else //La cámara debe girar
+        {
+            pc_NewPos = transform.position;
+            pc_NewPos = (pc_distanceFromObject * pc_NewPos) / pc_NewPos.magnitude;
+            pc_LastPos = pc_NewPos;
+        }
+        if(!Input.anyKey && pc_angle<10)
+        {
+            pc_NewPos = new Vector3(0, pc_CameraHeight, pc_distanceFromObject);
+        }
+        print(pc_angle);
+
+        return pc_NewPos;
     }
 }
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    // Use this for initialization
+
+    Vector3 direction;
+    public float aceleration = 0.015f;
+    public float MaxSpeed = 0.5f;
+    public GameObject Camera;
+    Vector3 CameraLookAt;
+    Vector3 CameraRight;
+    public float LoseFactor = 8f;
+
+    void Awake () {
+        direction = Vector3.zero;
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(-90, 0, 0);
+        CameraLookAt = Camera.transform.forward.normalized;
+        CameraRight = Camera.transform.right.normalized;
+
+        CameraLookAt = Vector3.ProjectOnPlane(CameraLookAt, new Vector3(0, 1, 0));
+        CameraRight = Vector3.ProjectOnPlane(CameraRight, new Vector3(0, 1, 0));
+
+        if (Input.GetKey("w"))
+        {
+            direction += CameraLookAt*aceleration;
+        } else 
+        {
+            direction -= CameraLookAt * aceleration*LoseFactor;
+        }
+        if (Input.GetKey("s"))
+        {
+            direction -= CameraLookAt*aceleration;
+        } else
+        {
+            direction += CameraLookAt * aceleration*LoseFactor;
+        }
+        if (Input.GetKey("d"))
+        {
+            direction += CameraRight * aceleration;
+        } else
+        {
+            direction -= CameraRight * aceleration*LoseFactor;
+        }
+        if (Input.GetKey("a"))
+        {
+            direction -= CameraRight * aceleration;
+        } else
+        {
+            direction += CameraRight * aceleration*LoseFactor;
+        }
+        if (!Input.anyKey)
+        {
+            direction -= direction*aceleration*5;
+            if (direction.magnitude < 0.05f)
+            {
+                direction = Vector3.zero;
+            }
+        }
+
+        if(direction.magnitude > MaxSpeed)
+        {
+            direction = (direction * MaxSpeed) / direction.magnitude;
+        }
+        if(direction.magnitude>0)
+            transform.forward = direction.normalized;
+
+        transform.position += direction;
+        //print(direction);
+    }
+}
